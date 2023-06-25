6bc9df49d230d92b5a359243173ad43d87427758
pujaribalaji <pujaribalaji152@gmail.com>
2023-06-25 22:56:31 +0530

    commit

--- models/database.js
+++ models/database.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
 require('dotenv').config();
-mongoose.connect('mongodb+srv://balaji152:balaji152@cluster0.nxp1t75.mongodb.net/Adload?retryWrites=true&w=majority' ,{ useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 const db = mongoose.connection;
 db.on('error',console.error.bind(console, 'connection error:'));
